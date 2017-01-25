60aea3ae7ddc81069fe79943d0319f6dda8cccb3
Laurent Gaches <laurent@binimo.com>
2017-01-25 22:29:43 +0100

    add ruby

---
+++ Performance/Ruby/main.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'mongo'
+
+Mongo::Logger.logger.level = ::Logger::INFO
+
+startTime = Time.now
+
+mongoClient = Mongo::Client.new(['localhost:27017'], :database => 'compare')
+collection = mongoClient[:users]
+
+documents = Array.new(10000, { name_first: 'Joannis', 
+                        name_last: 'Orlandos', 
+                        age: 20, 
+                        programmer:true, 
+                        likes: ["mongodb", "swift", "programming", "swimming"] })
+
+collection.insert_many(documents)
+
+otherDocuments = Array.new(10000,{ name_first: 'Harriebob', 
+                        name_last: 'Narwhal', 
+                        age: 42, 
+                        programmer:false, 
+                        likes: ["facebook", "golfing", "cooking", "reading"] })
+collection.insert_many(otherDocuments)                        
+
+counter = 0 
+collection.find({'age' => { '$gt' => 18}}).each { 
+    counter +=1
+}
+puts counter
+
+counter2 = 0
+collection.find({'name_first' => 'Joannis'}).each {
+    counter2 += 1
+}
+puts counter2
+
+
+collection.delete_many({})
+
+endTime = Time.now
+
+puts endTime - startTime
