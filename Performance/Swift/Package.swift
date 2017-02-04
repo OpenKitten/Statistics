9908a0bc4630d51398d1ad2453be6c6182e86c9d
Laurent Gaches <laurent@binimo.com>
2017-02-04 11:58:33 +0100

    update mongokitten version

--- Performance/Swift/Package.swift
+++ Performance/Swift/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftPerformance",
     dependencies: [
-        .Package(url: "https://github.com/OpenKitten/MongoKitten.git", "3.1.0-beta2")
+        .Package(url: "https://github.com/OpenKitten/MongoKitten.git", majorVersion: 3)
     ]
 )
